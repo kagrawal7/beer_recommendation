50939f09146389062c40e91379f414dc6227861a
kagrawal7 <k.agrawal@mail.utoronto.ca>
2022-09-07 12:27:16 +0530

    added comments and main statement

---
+++ knn.py
@@ -0,0 +1,10 @@
+# knn
+
+
+if __name__ == '__main__':
+    # run knn
+    # take a list of beers a reviewer has reviewed (good and bad),
+    # pick one of the beers and predict the rating the reviewer
+    # gave that beer, based off of similarity to other beers that were reviewed
+    # will have to fully understand scikit learn capabilities
+    temp = 3
